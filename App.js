aa1c435508ba35dbef0de5ab93de12ce4f8b24fe
Mohd Ebrahim <mohd.ebrahim.jmi@gmail.com>
2024-01-30 19:21:25 +0530

    go through parcel, npm, npx, package.json, package-lock.json

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
-{/* <div id="parent">
-  <div id="child">
-    <h1>I am a child</h1>
-    <h2>zasddf</h2>
-  </div>
-</div> */}
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const heading = React.createElement('div', {id: 'parent'}, React.createElement('div', {id: 'child'}, [React.createElement('h1', {}, 'I am h1 tag'), React.createElement('h2', {}, 'I am h1 tag')]));
 
-
-console.log(heading);
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
